template_combined: Remove incorrect naive Chi_4 variance calculation

template_combined.py
@@ -17,7 +17,6 @@ def usage():
         "-q Scattering vector constant (default: 7.25)",
         "-o Start index (from 1) of particles to limit analysis to",
         "-p End index (from 1) of particles to limit analysis to",
-        "-z Output variance for naive chi_4 instead of standard deviation during multi-run analysis",
         "-h Print usage",
         "Interval increase progression (last specified is used):",
         "-f Flenner-style periodic-exponential-increasing increment (iterations: 50, power: 5)",
@@ -25,7 +24,7 @@ def usage():
         sep="\n", file=sys.stderr)
 
 try:
-  opts, args = getopt.gnu_getopt(sys.argv[1:], "n:r:s:k:m:d:a:q:o:p:zhfg:")
+  opts, args = getopt.gnu_getopt(sys.argv[1:], "n:r:s:k:m:d:a:q:o:p:hfg:")
 except getopt.GetoptError as err:
   print(err, file=sys.stderr)
   usage()
@@ -61,8 +60,6 @@ upper_limit = None
 lower_limit = None
 # Type of progression to increase time interval by
 progtype = progtypes.flenner
-# Whether to calculate chi_4 variance instead of standard deviation
-variance_opt = False
 
 for o, a in opts:
   if o == "-h":
@@ -91,16 +88,11 @@ for o, a in opts:
   elif o == "-p":
     limit_particles = True
     upper_limit = int(a)
-  elif o == "-z":
-    variance_opt = True
   elif o == "-f":
     progtype = progtypes.flenner
   elif o == "-g":
     progtype = progtypes.geometric
     geom_base = float(a)
-
-if variance_opt == True and n_runs == 1:
-  print("Warning: Variance calculation specified but only one run to be used", file=sys.stderr)
 
 # Holds number of frames per file
 fileframes = np.empty(n_files + 1, dtype=int)
@@ -269,8 +261,8 @@ run_overlap = np.empty(n_samples, dtype=float)
 run_fc = np.empty((n_samples, 4), dtype=float)
 
 if rundirs == True:
-  # Corresponding arrays used for calculating standard deviations (or
-  # variances) across runs
+  # Corresponding arrays used for calculating standard deviations
+  # across runs
   std_msd = np.zeros(n_samples, dtype=float)
   std_overlap = np.zeros(n_samples, dtype=float)
   std_fc = np.zeros((n_samples, 4), dtype=float)
@@ -392,17 +384,11 @@ if rundirs == True:
   std_msd /= n_runs
   std_overlap /= n_runs
 
-  if variance_opt == False:
-    # Calculate standard deviation with means and means of squares of
-    # values
-    std_fc = np.sqrt((std_fc - fc**2) / (n_runs - 1))
-    std_msd = np.sqrt((std_msd - msd**2) / (n_runs - 1))
-    std_overlap = np.sqrt((std_overlap - overlap**2) / (n_runs - 1))
-  else:
-    # Calculate variance with means and means of squares of values
-    std_fc = particles * (std_fc - fc**2)
-    std_msd = particles * (std_msd - msd**2)
-    std_overlap = particles * (std_overlap - overlap**2)
+  # Calculate standard deviation with means and means of squares of
+  # values
+  std_fc = np.sqrt((std_fc - fc**2) / (n_runs - 1))
+  std_msd = np.sqrt((std_msd - msd**2) / (n_runs - 1))
+  std_overlap = np.sqrt((std_overlap - overlap**2) / (n_runs - 1))
 
 print("#dt = %f" %framediff)
 print("#q = %f" %q)
@@ -413,10 +399,10 @@ for i in range(0, n_samples):
   if rundirs == True:
     # Print time difference, msd, averarge overlap, x, y, and z
     # scattering function averages, average of directional scattering
-    # functions, standard deviations (or variances) of msd, averarge
-    # overlap, x, y, and z scattering function averages, average of
-    # directional scattering function standard deviations, number of
-    # frame sets contributing to such averages, and frame difference
+    # functions, standard deviations of msd, averarge overlap, x, y,
+    # and z scattering function averages, average of directional
+    # scattering function standard deviations, number of frame sets
+    # contributing to such averages, and frame difference
     print("%f %f %f %f %f %f %f %f %f %f %f %f %f %d %d" %(time,
                                                            msd[i],
                                                            overlap[i],